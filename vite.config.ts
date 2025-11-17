9ad4a971f323e0cd0375db84798ca58a0905ffc3
onepercman <onepercman@gmail.com>
2025-11-17 11:46:17 +0700

    🚀 update base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from "@tailwindcss/vite"
 import { devtools } from "@tanstack/devtools-vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
-import viteReact from "@vitejs/plugin-react"
+import viteReact from "@vitejs/plugin-react-swc"
 import { nitro } from "nitro/vite"
 import { defineConfig } from "vite"
 import viteTsConfigPaths from "vite-tsconfig-paths"
